e5c0da189d085b48d3bb01ca46f1c1ad1779050d
zhouByte-hub <zhou_jia_ning@163.com>
2026-07-12 18:21:12 +0800

    初始化Scorpio-netty模块

---
+++ scorpio-netty/src/test/java/com/zhoubyte/scorpionetty/ScorpioNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoubyte.scorpionetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorpioNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
